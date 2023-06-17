7b9c3601f181f91dcd8edbdb3d15616604c79189
Jonathan <jbecker24@cmc.edu>
2023-06-17 15:14:06 -0700

    Fixed the controller issues.. works with PDFs now but not images. The view is very ugly

--- Game Controller File Viewer/ContentView.swift
+++ Game Controller File Viewer/ContentView.swift
@@ -43,6 +43,9 @@ struct DocumentPicker: UIViewControllerRepresentable {
 
 struct PDFKitView: UIViewRepresentable {
     let url: URL
+    @Binding var currentPage: Int
+    @Binding var scale: CGFloat
+    @Binding var offset: CGFloat
 
     func makeUIView(context: Context) -> PDFView {
         let pdfView = PDFView()
@@ -54,15 +57,22 @@ struct PDFKitView: UIViewRepresentable {
 
     func updateUIView(_ uiView: PDFView, context: Context) {
         uiView.document = PDFDocument(url: url)
-        uiView.usePageViewController(true, withViewOptions: [UIPageViewController.OptionsKey.interPageSpacing: 20])
+        let page = uiView.document?.page(at: currentPage) ?? PDFPage()
+        uiView.go(to: page)
+        uiView.scaleFactor = uiView.scaleFactorForSizeToFit * scale
+        uiView.go(to: CGRect(x: 0, y: offset, width: 1, height: 1), on: page)
     }
 }
 
 struct ContentView: View {
     @State private var importedPDFs: [URL] = []
     @State private var showDocumentPicker = false
-    @State private var currentPDFIndex = 0
+    @State private var currentPage = 0
+    @State private var scale: CGFloat = 1.0
+    @State private var offset: CGFloat = 0.0
     @State private var currentController: GCController?
+    @State private var timer: Timer? = nil
+    @State private var lastActionTime = Date()
 
     var body: some View {
         VStack {
@@ -71,14 +81,8 @@ struct ContentView: View {
                     .font(.largeTitle)
                     .foregroundColor(.gray)
             } else {
-                TabView(selection: $currentPDFIndex) {
-                    ForEach(importedPDFs.indices, id: \.self) { index in
-                        PDFKitView(url: importedPDFs[index])
-                            .tag(index)
-                    }
-                }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                .frame(height: UIScreen.main.bounds.height * 0.7)
+                PDFKitView(url: importedPDFs.first!, currentPage: $currentPage, scale: $scale, offset: $offset)
+                    .frame(height: UIScreen.main.bounds.height * 0.7)
             }
 
             Button(action: {
@@ -94,31 +98,51 @@ struct ContentView: View {
         }
         .padding()
         .onAppear {
-            let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
                 if self.currentController == nil {
                     self.currentController = GCController.controllers().first
-                }
-                
-                DispatchQueue.main.async {
-                    if self.currentController?.extendedGamepad?.dpad.right.isPressed == true {
-                        if self.currentPDFIndex < self.importedPDFs.count - 1 {
-                            self.currentPDFIndex += 1
-                        }
-                    } else if self.currentController?.extendedGamepad?.dpad.left.isPressed == true {
-                        if self.currentPDFIndex > 0 {
-                            self.currentPDFIndex -= 1
+
+                    self.currentController?.extendedGamepad?.buttonMenu.valueChangedHandler = { (button, value, pressed) in
+                        if pressed {
+                            self.showDocumentPicker = true
                         }
                     }
                 }
 
-                self.currentController?.extendedGamepad?.buttonMenu.valueChangedHandler = { (button, value, pressed) in
-                    if pressed {
-                        self.showDocumentPicker = true
+                let now = Date()
+                if now.timeIntervalSince(self.lastActionTime) > 0.5 {
+                    DispatchQueue.main.async {
+                        if self.currentController?.extendedGamepad?.dpad.right.isPressed == true {
+                            self.currentPage += 1
+                            self.lastActionTime = now
+                        } else if self.currentController?.extendedGamepad?.dpad.left.isPressed == true {
+                            self.currentPage -= 1
+                            if self.currentPage < 0 {
+                                self.currentPage = 0
+                            }
+                            self.lastActionTime = now
+                        } else if self.currentController?.extendedGamepad?.buttonA.isPressed == true {
+                            self.scale *= 1.1
+                            self.lastActionTime = now
+                        } else if self.currentController?.extendedGamepad?.buttonB.isPressed == true {
+                            self.scale /= 1.1
+                            self.lastActionTime = now
+                        } else if self.currentController?.extendedGamepad?.dpad.up.isPressed == true {
+                            self.offset += 50
+                            self.lastActionTime = now
+                        } else if self.currentController?.extendedGamepad?.dpad.down.isPressed == true {
+                            self.offset -= 50
+                            self.lastActionTime = now
+                        }
                     }
                 }
             }
-            timer.tolerance = 0.05
-            RunLoop.current.add(timer, forMode: .common)
+            timer?.tolerance = 0.05
+            RunLoop.current.add(timer!, forMode: .common)
+        }
+        .onDisappear {
+            timer?.invalidate()
+            timer = nil
         }
     }
 }
